684f2673858e740db2184ab7cdbc91dd598386ff
Adam10108 <Adam_Atlas1998@hotmail.com>
2020-01-03 20:01:54 +0700

    create routes.js

---
+++ src/containers/homepage/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Index() {
+  return <>HomePage</>;
+}
+export default Index;
--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Routes from '../core/routes/routes';
+
 function App() {
-  return <>App.js</>;
+  return (
+    <>
+      <Routes />
+    </>
+  );
 }
 
 export default App;
