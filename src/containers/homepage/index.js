0099c6838e110c2cc028fd8506e956f6da2e0f4c
Adam10108 <Adam_Atlas1998@hotmail.com>
2020-01-03 23:30:33 +0700

    add : header and layout component

--- src/containers/homepage/index.js
+++ src/containers/homepage/index.js
@@ -1,6 +1,16 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
+import Layout from '../../components/layout'
+
+const ContentLayout = styled.div`
+  /* height: 100; */
+`
 function Index() {
-  return <>HomePage</>
+  return (
+    <Layout>
+      <ContentLayout>Content</ContentLayout>
+    </Layout>
+  )
 }
 export default Index
