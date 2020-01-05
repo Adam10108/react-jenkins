8cd3484d5f0431dd5f2bc213504f43b1b2f3df5d
Adam10108 <Adam_Atlas1998@hotmail.com>
2020-01-06 01:03:27 +0700

    create add and show users

--- src/containers/homepage/index.js
+++ src/containers/homepage/index.js
@@ -1,15 +1,40 @@
+import { Row, Col } from 'antd'
+import * as R from 'ramda'
 import React from 'react'
 import styled from '@emotion/styled'
 
+import { getUsers } from '../../core/models/users'
+import FormAddUsers from './components/formAddUser'
 import Layout from '../../components/layout'
+import Table from '../../components/table'
+import useQuery from '../../core/hooks/useQuery'
 
-const ContentLayout = styled.div`
-  /* height: 100; */
+const Content = styled.div`
+  margin: 30px 8.22%;
+  height: 80%;
 `
+
 function Index() {
+  const resp = useQuery(getUsers)
+
   return (
     <Layout>
-      <ContentLayout>Content</ContentLayout>
+      <Content>
+        <Row gutter={20}>
+          <Col lg={9}>
+            <FormAddUsers
+              setIsLoading={R.path(['setIsLoading'], resp)}
+              reloadData={R.path(['reloadData'], resp)}
+            />
+          </Col>
+          <Col lg={15}>
+            <Table
+              isLoading={R.path(['isLoading'], resp)}
+              data={R.path(['data'], resp)}
+            />
+          </Col>
+        </Row>
+      </Content>
     </Layout>
   )
 }
