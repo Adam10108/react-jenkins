33e0ed530199a926ca4ae054c1cbf8f9c5651b94
Adam10108 <Adam_Atlas1998@hotmail.com>
2020-01-06 01:01:36 +0700

    add : component table

---
+++ src/components/table/index.js
@@ -0,0 +1,46 @@
+import { Table as AntdTable } from 'antd'
+
+import React from 'react'
+import styled from '@emotion/styled'
+
+const StyledText = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  opacity: 0.7;
+  color: 'red';
+`
+
+const columns = [
+  {
+    title: 'Email',
+    dataIndex: 'email',
+    key: 'email',
+    render: email => {
+      return <StyledText>{email}</StyledText>
+    }
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    render: name => {
+      return <StyledText>{name}</StyledText>
+    }
+  },
+  {
+    title: 'Phone Number',
+    dataIndex: 'phoneNumber',
+    key: 'phoneNumber',
+    render: phoneNumber => {
+      return <StyledText>{phoneNumber}</StyledText>
+    }
+  }
+]
+
+const Table = props => {
+  const { isLoading, data } = props
+  console.log('isLoading : ', isLoading)
+  return <AntdTable loading={isLoading} columns={columns} dataSource={data} />
+}
+
+export default Table
